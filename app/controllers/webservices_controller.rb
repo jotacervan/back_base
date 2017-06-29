182a3c4fa9f5db51e8d84edfb4065663fc64c9e5
Jota Cervan <jota@mobile2you.com.br>
2017-06-29 09:57:25 -0300

    Login

--- app/controllers/webservices_controller.rb
+++ app/controllers/webservices_controller.rb
@@ -3,7 +3,7 @@ class WebservicesController < ApplicationController
 	before_action :check_login, except: [:signin, :signup, :signout]
 	
 	def check_login
-		if user_signed_in?
+		if !user_signed_in?
 			render :json => { :message => 'Faça o seu login para continuar' }, status: 401
 		end
 	end
