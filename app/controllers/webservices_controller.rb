bfd6039152e086fdb6ed69f9b3817ba3e6ce272a
Jota Cervan <jota@mobile2you.com.br>
2017-06-30 15:21:05 -0300

    teste

--- app/controllers/webservices_controller.rb
+++ app/controllers/webservices_controller.rb
@@ -3,7 +3,7 @@ class WebservicesController < ApplicationController
 	before_action :check_login, except: [:signin, :signup, :update_question, :update_photos, :getTimes, :getTorcidas, :update_address , :complete_login, :set_payment ]
 	
 	def check_login
-		if !user_signed_in?
+		if current_user.nil?
 			render :json => { :message => 'Faça o seu login para continuar' }, status: 401
 		end
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
   	# 	   QUESTION METHODS 
   	# ===========================
   	get '/questions/getQuestion'
-
+    
     # ===========================
     #      TIME METHODS
     # ===========================
